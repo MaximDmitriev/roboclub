2fed8722746bccfe4982cf3eba87f23edf84e4f4
Maxim Dmitriev <maximv.dmitriev@gmail.com>
2018-11-28 11:42:49 +0300

    maps added

--- js/main.js
+++ js/main.js
@@ -10,7 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
         main = document.querySelector(".main"),
         masterclass = document.querySelector(".masterclass"),
         program = document.querySelector(".program"),
-        experts = document.querySelector(".experts");
+        experts = document.querySelector(".experts"),
+        contacts = document.querySelector(".contacts");
 
     homeBtn.addEventListener('click', () => {
         header.scrollIntoView({block:"start", behavior: "smooth"});
@@ -43,7 +44,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 experts.scrollIntoView({block:"start", behavior: "smooth"});
                 break;
 
-
+                case 5:
+                contacts.scrollIntoView({block:"start", behavior: "smooth"});
+                break;
             }
         });
     });
@@ -170,14 +173,36 @@ document.addEventListener('DOMContentLoaded', () => {
         let myMap1 = new ymaps.Map("mapBibirevo", {
         //   center: [55.894882, 37.610855],
           center: [55.8948, 37.6108],
+          controls: [],
           zoom: 17
         });
         let myMap2 = new ymaps.Map("mapVdnh", {
-            //   center: [55.894882, 37.610855],
-              center: [55.8178, 37.6354],
-              zoom: 17
+        //   center: [55.894882, 37.610855],
+            center: [55.8178, 37.6354],
+            controls: [],
+            zoom: 17
+        });
+
+        let placeBibirevo = new ymaps.Placemark([55.8948, 37.6108],
+            {
+                iconContent: "РОББО Клуб"
+            },
+            {
+                preset: 'islands#darkGreenStretchyIcon'
+    
             });
-      };
+        myMap1.geoObjects.add(placeBibirevo);
+
+        let placeVdnh = new ymaps.Placemark([55.8178, 37.6354],
+            {
+                iconContent: "РОББО Клуб"
+            },
+            {
+                preset: 'islands#darkGreenStretchyIcon'
+    
+            });
+        myMap2.geoObjects.add(placeVdnh);
+      }
+
 });
 
-// 55.817826, 37.635417
