9b21237cf311735e0bf8f1b58220ab035c88a58f
Maxim Dmitriev <maximv.dmitriev@gmail.com>
2018-11-29 23:56:43 +0300

    intagram added

--- js/main.js
+++ js/main.js
@@ -158,16 +158,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // maps
 
-    let divMapBibirevo = document.querySelector("#mapBibirevo"),
-        divMapVdnh = document.querySelector("#mapVdnh"),
-        k = "b59c759a-af40-45f1-8a1f-1e61f0009b9f";
+    // let divMapBibirevo = document.querySelector("#mapBibirevo"),
+    //     divMapVdnh = document.querySelector("#mapVdnh"),
+    //     k = "b59c759a-af40-45f1-8a1f-1e61f0009b9f";
 
-    // function init(ymaps){
-    //     var mapBibirevo = new ymaps.Map("mapBibirevo", {
-    //         center:[55.89, 37.61],
-    //         zoom: 10
-    //     });
-    // }
     ymaps.ready(init);
     function init () {
         let myMap1 = new ymaps.Map("mapBibirevo", {
@@ -250,12 +244,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     });
 
-    // let i = 50;
-    // btnVknext.addEventListener('click', () =>{
-    //     vkSlide[vkPostNum].style.marginLeft = i + "px";
-    //     i+=50;
 
-    // });
 
     btnVkprev.addEventListener('click', () =>{
         vkSlide[vkPostNum].style.display = "none";
@@ -277,7 +266,66 @@ document.addEventListener('DOMContentLoaded', () => {
 
     });
 
+    // ig
 
+    let btnIgprev = document.querySelector(".ig-prev"),
+        btnIgnext = document.querySelector(".ig-next"),
+        igPostNum = 1,
+        urls = [
+        "p/Bhvhnoyhhs0/",
+        "p/BnsfxlLBPFe/",
+        "p/BnwSx75lKVR/",
+        "p/BodgLRVB3Wa/",
+        "p/Bht1fD7hMpq/",
+        // "p/BcagLIDlB9h/",
+        "p/BVq96PGFA0Z/",
+        "p/BbytzKMlLLH/"
+    ];
+
+    getIg(urls[0]);
+
+    btnIgnext.addEventListener('click', () => {
+        if(igPostNum == urls.length - 1){
+            igPostNum = 0;
+        } else{
+            igPostNum++;
+        }
+        getIg(urls[igPostNum]);
+    });
+
+    btnIgprev.addEventListener('click', () => { //пофиксить первый клик
+        if(igPostNum == 0){
+            igPostNum = urls.length - 1;
+        } else{
+            igPostNum--;
+        }
+        getIg(urls[igPostNum]);
+    });
+
+    function getIg(url){
+        let divIg = document.querySelector(".ig-post");
+        divIg.style.transition = "0.5s";
+
+        let request = new XMLHttpRequest();
+
+        request.open("GET", "https://api.instagram.com/oembed?url=http://instagr.am/" + url + "&maxwidth=320&omitscript=true&hidecaption=true");
+        request.send();
+
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState == 4 && request.status == 200){
+                let data = JSON.parse(request.response);
+                // console.log(data.html);
+                divIg.style.opacity = 0;
+                setTimeout(() => {
+                    
+                    divIg.innerHTML = data.html;
+                    instgrm.Embeds.process();
+                    setTimeout(() => {divIg.style.opacity = 1;}, 600);
+                }, 500);
+            }
+
+        });
+    }
 
     // modal form
 
